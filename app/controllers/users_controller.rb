45fd75b7d827530c3ffa02439af86638b6ff852f
yoshiko <yoshikouki@gmail.com>
2020-01-12 14:09:50 +0000

    Finish toy app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User
-    @microposts = @user.microposts
   end
 
   # GET /users/new
